7f5035b81f6e78f83274cdfb8df0a5b8d01f22c7
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-06 19:07:22 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
       <div class="flex flex-col items-left lg:flex-row lg:items-start justify-center">
         <div class="text-center mt-4 lg:mr-6">
         <hr className="w-25 mt-4 mb-4 border-t-2 border-dashed border-slate-600 mx-auto" />
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3248.6270420091646!2d88.39372604222423!3d22.666387697586085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1704547687204!5m2!1sen!2sin" width="320" height="300" allowfullscreen="" loading="lazy"></iframe>
+        <iframe className="rounded-lg border-cyan-500 border-2" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3248.6270420091646!2d88.39372604222423!3d22.666387697586085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1704547687204!5m2!1sen!2sin" width="320" height="300" allowfullscreen="" loading="lazy"></iframe>
           <div className="mt-4 relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-cyan-400">
             {locationString}
           </div>
