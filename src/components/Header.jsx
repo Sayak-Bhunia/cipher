a593a8c793d5a838e9e78bb53a78658135b642e4
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-07 10:45:30 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,34 @@ import React, { useState, useEffect } from 'react';
 
 const Header = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [batteryStatus, setBatteryStatus] = useState(null);
+  const [onlineStatus, setOnlineStatus] = useState(navigator.onLine);
 
   useEffect(() => {
+    // Battery status
+    navigator.getBattery().then((battery) => {
+      setBatteryStatus(battery.level * 100);
+      battery.addEventListener('levelchange', () => {
+        setBatteryStatus(battery.level * 100);
+      });
+    });
 
+    // Online/offline status
+    const handleOnlineStatusChange = () => {
+      setOnlineStatus(navigator.onLine);
+    };
+
+    window.addEventListener('online', handleOnlineStatusChange);
+    window.addEventListener('offline', handleOnlineStatusChange);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('online', handleOnlineStatusChange);
+      window.removeEventListener('offline', handleOnlineStatusChange);
+    };
+  }, []);
+
+  useEffect(() => {
     const timeIntervalId = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
@@ -19,17 +44,23 @@ const Header = () => {
 
   return (
     <>
-      <div class="flex flex-col items-left lg:flex-row lg:items-start justify-center">
-        <div class="text-center lg:mr-6">
+      <div className="flex flex-col items-left lg:flex-row lg:items-start justify-center">
+        <div className="text-center lg:mr-6">
           <div className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-cyan-400">
             {formattedTime}
             <span className="text-custom">hshsjsjsjsjssj</span>
             {formattedDate}
+            <br />
+            {batteryStatus !== null && (
+              <span>power: {batteryStatus.toFixed(2)}%</span>
+            )}
+            <span className="text-custom">hshsjsjsjsjssj</span>
+            <span>{onlineStatus ? 'Online' : 'Offline'}</span>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default Header;
