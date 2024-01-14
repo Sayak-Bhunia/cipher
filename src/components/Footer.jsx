ee3eca49a13e3482f0ae812381ecc5315c7f7ee3
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-14 19:53:16 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
         <div class="text-center mt-4 lg:mr-6">
         <hr className="w-25 mt-4 mb-4 border-t-2 border-dashed border-slate-500 mx-auto" />
         <iframe className="rounded-lg border-slate-500 border-2" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6999.268971517835!2d88.38936975383761!3d22.660974471043875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1704791147954!5m2!1sen!2sin" width="320" height="300" allowfullscreen="" loading="lazy"></iframe>
-          <div className="mt-4 relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-cyan-400">
+          <div className="mt-4 relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-slate-500">
             {locationString}
           </div>
         </div>
