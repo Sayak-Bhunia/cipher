3fd5b74bb48e1aa98d2992217c60d578e1413f11
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-14 23:25:15 +0530

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,7 +11,7 @@ const Content = () => {
       <>
         <div className="flex flex-col items-left lg:flex-row lg:items-start justify-center">
           <div className="text-left mt-2 lg:mr-6">
-            <img src="./Heading2.png" className="w-96" alt="Heading" />
+            <img src="./Heading3.png" className="w-96" alt="Heading" />
             <progress className="progress w-84"></progress>
             <h3 className="text-cyan-400 text-lg font-semibold tracking-tight ml-0s">
               <a
@@ -25,7 +25,7 @@ const Content = () => {
             <h3 className="text-slate-500 text-md font-semibold tracking-tight ml-0s">
               don't be afraid. be brave.
             </h3>
-            <h3 className="text-cyan-400 text-lg font-semibold tracking-tight mt-4 ml-0s">
+            <h3 className="text-neutral-200 text-lg font-semibold tracking-tight mt-4 ml-0s">
               <a
                 href="https://drive.google.com/drive/folders/1O7WVpqd5f4pYk5AelpoKtF2f_d1jdWrj?usp=sharing"
                 target="_blank"
@@ -37,7 +37,7 @@ const Content = () => {
             <h3 className="text-slate-500 text-md font-semibold tracking-tight ml-0">
               there u go.
             </h3>
-            <h3 className="text-cyan-400 text-lg font-semibold tracking-tight mt-4 ml-0s">
+            <h3 className="text-neutral-200 text-lg font-semibold tracking-tight mt-4 ml-0s">
               <a
                 href="https://leetcode.com/contest/"
                 target="_blank"
@@ -49,7 +49,7 @@ const Content = () => {
             <h3 className="text-slate-500 text-md font-semibold tracking-tight ml-0">
               leetcode schedule.
             </h3>
-            <h3 className="text-cyan-400 text-lg font-semibold tracking-tight mt-4 ml-0s">
+            <h3 className="text-nuetral-200 text-lg font-semibold tracking-tight mt-4 ml-0s">
               <a
                 href="https://www.codechef.com/contests"
                 target="_blank"
@@ -61,7 +61,7 @@ const Content = () => {
             <h3 className="text-slate-500 text-md font-semibold tracking-tight ml-0">
               codechef schedule.
             </h3>
-            <h3 className="text-cyan-400 text-lg font-semibold tracking-tight mt-4 ml-0s">
+            <h3 className="text-nuetral-200 text-lg font-semibold tracking-tight mt-4 ml-0s">
               <a
                 href="https://codeforces.com/contests"
                 target="_blank"
@@ -73,7 +73,7 @@ const Content = () => {
             <h3 className="text-slate-500 text-md font-semibold tracking-tight ml-0">
               codeforces schedule.
             </h3>
-            <h3 className="text-cyan-400 text-lg font-semibold tracking-tight mt-4 ml-0s">
+            <h3 className="text-nuetral-200 text-lg font-semibold tracking-tight mt-4 ml-0s">
               <a
                 href="https://drive.google.com/file/d/1xpnDPhctOv7fTw0ILMJ074NXe3GnHbil/view?usp=sharing"
                 target="_blank"
